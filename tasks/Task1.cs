948115b99f7ff4209d19796d3c4bb4ab37e0b39f
Gitreposuser <vlad77solo@gmail.com>
2022-04-04 14:12:49 +0200

    First commit

---
+++ tasks/Task1.cs
@@ -0,0 +1,116 @@
+﻿public class Task1
+{
+	public Task1()
+	{
+        // First list
+        ListNode list111 = new ListNode(1, null);
+        ListNode list11 = new ListNode(3, list111);
+        ListNode list1 = new ListNode(4, list11);
+        ListNode curNode1 = new ListNode(2, list1);
+
+        // Second list
+        ListNode list22 = new ListNode(9, null);
+        ListNode list2 = new ListNode(9, list22);
+        ListNode curNode2 = new ListNode(9, list2);
+
+        ShowNodes(curNode1);
+        ShowNodes(curNode2);
+
+        ListNode result = AddTwoNumbers(curNode1, curNode2);
+
+        ShowNodes(result);
+    }
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode result = new ListNode();
+
+        ListNode buff1 = l1,
+                 buff2 = l2,
+                 curNode = result,
+                 prevNode = result,
+                 emptyNode = new ListNode(0, null);
+
+        int sum;
+        bool inMemmory = false;
+
+        do
+        {
+            // 1st stage get sum of two nodes
+            if (buff1 == null)
+            {
+                buff1 = emptyNode;
+                sum = buff2.val;
+            }
+            if (buff2 == null)
+            {
+                buff2 = emptyNode;
+                sum = buff1.val;
+            }
+            else
+                sum = buff1.val + buff2.val;
+
+            // 2nd stage when already in memmory
+            if (inMemmory)
+            {
+                inMemmory = false;
+                ++sum;
+            }
+
+            // 3nd stage check overflow and write
+            // to resulting list
+            if (sum > 9)
+            {
+                inMemmory = true;
+                sum -= 10;
+            }
+
+            // Set value to current node
+            curNode.val = sum;
+
+            // Create new node and set as next
+            ListNode tempNode = new ListNode();
+            curNode.next = tempNode;
+
+            // Set last node
+            prevNode = curNode;
+
+            // Change current node to next
+            curNode = tempNode;
+
+            // 4rd stage take next numbers from nodes
+            buff1 = buff1.next;
+            buff2 = buff2.next;
+        }
+        while ((buff1 != null) ||
+               (buff2 != null));
+
+        if (inMemmory)
+            curNode.val = 1;
+        else
+            prevNode.next = null;
+
+        return result;
+    }
+    void ShowNodes(ListNode node)
+    {
+        Console.Write(" node: " + node.val);
+        node = node.next;
+
+        while (node != null)
+        {
+            Console.Write($", {node.val}");
+            node = node.next;
+        }
+        Console.WriteLine("");
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int ival = 0, ListNode inext = null)
+        {
+            val = ival;
+            next = inext;
+        }
+    }
+}
---
+++ tasks/Task2.cs
@@ -0,0 +1,96 @@
+﻿public class Task2
+{
+	public Task2()
+	{
+        ListNode l1 = new ListNode(9, null);
+        //l1 = new ListNode(9, l1);
+        //l1 = new ListNode(9, l1);
+
+        ListNode l2 = new ListNode(9, null);
+        //l2 = new ListNode(9, l2);
+        //l2 = new ListNode(9, l2);
+        //l2 = new ListNode(9, l2);
+        //l2 = new ListNode(9, l2);
+
+        ShowNodes(l1);
+        ShowNodes(l2);
+
+        ListNode result = AddTwoNumbers(l1, l2);
+
+        ShowNodes(result);
+    }
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode result = null;
+        byte onePlus = 0;
+        byte node1;
+        byte node2;
+        byte sumDigits = 0;
+
+        while ((l1 != null) || (l2 != null))
+        {
+            if (l1 == null)
+                node1 = 0;
+            else
+            {
+                node1 = (byte)l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 == null)
+                node2 = 0;
+            else
+            {
+                node2 = (byte)l2.val;
+                l2 = l2.next;
+            }
+
+            sumDigits = (byte)(node1 + node2 + onePlus);
+
+            if (sumDigits > 9)
+            {
+                sumDigits = (byte)(sumDigits % 10);
+                onePlus = 1;
+            }
+            else
+            {
+                sumDigits = sumDigits;
+                onePlus = 0;
+            }
+
+            result = new ListNode(sumDigits, result);
+        }
+
+        if (onePlus == 1)
+            result = new ListNode(1, result);
+
+        // Reverse list
+        ListNode reversed = null;
+        while (result != null)
+        {
+            reversed = new ListNode(result.val, reversed);
+            result = result.next;
+        }
+
+        return reversed;
+    }
+    public static void ShowNodes(ListNode node)
+    {
+        Console.WriteLine(" *** list node ***");
+        while (node != null)
+        {
+            Console.WriteLine(" val = " + node.val);
+            node = node.next;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
